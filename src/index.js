07470f1575c55376b552e0938466de830291cca0
Jules2791 <may.julie@yahoo.co.uk>
2025-03-07 08:00:05 +0000

    Update to AI context to ensure it uses basic HTML

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "ba5f0o41c7ff3cc032eee04b3d66at0a";
   let prompt = `User instructions: Generate a poem about ${instructionInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML. Example <p>poem goes here</p> and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
